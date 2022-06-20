8ead01e78dbf93509ccc7086d680083d52e81d94
depressionmelon07 <107890630+depressionmelon07@users.noreply.github.com>
2022-06-21 09:23:45 +1200

    Add files via upload

---
+++ 00_base_v0.py
@@ -0,0 +1,15 @@
+"""
+Base component for the game Lucky Unicorn 
+V0 - skeleton setup 
+
+author - Pari Rao 
+CC PR 2022
+"""
+
+# import libraries below this line ******************************
+
+# all functions below this line *********************************
+
+
+# main routine below this line **********************************
+print("Hello")
