f380fcbbe606e9443dba7700ee42b8fe8d31315b
depressionmelon07 <parnikarao@pnghs.school.nz>
2022-07-26 12:45:56 +1200

    New token generator with comments

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -12,15 +12,25 @@ for item in range(0, 10):
     chosen_num = random.randint(1, 100)
 
     # Adjust balance
+    # if the random # is between 1 and 5,
+    # user gets a unicorn (add $4 to balance)
     if 1 <= chosen_num <= 5:
-        chosen == "unicorn"
+        chosen = "unicorn"
         balance += 4
+
+    # if the random # is between 6 and 36,
+    # user gets a donkey (minus $1 from balance)
     elif 6 <= chosen_num <= 36:
         chosen = "donkey"
         balance -= 1
+
+    # the token is either a zebra or a horse
+    # in both circumstances, subtract $0.50 from balance
     else:
+        # if the number is even, set the chosen item to a horse
         if chosen_num % 2 == 0:
             chosen = "horse "
+        # otherwise set to a zebra
         else:
             chosen = "zebra"
 
